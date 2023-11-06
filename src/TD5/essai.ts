8e0f57951d69484d5b5692d36bdf4dc40b7f1ce3
Diagne <cheikh.diagne2@etu.univ-lorraine.fr>
2023-11-06 11:26:19 +0100

    cours

--- src/TD5/essai.ts
+++ src/TD5/essai.ts
@@ -1,6 +1,11 @@
-let chaine = "George";
-console.log("de la fin vers 0");
-for (let i = chaine.length - 1; i >= 0; i--) {
-  chaine[i];
-  console.log(chaine[i]);
+let prenom = "George";
+console.log("Parcourir de 0 vers la fin");
+for (let i = 0; i < prenom.length; i++) {
+  console.log(prenom[i]);
+}
+
+console.log("\nde la fin vers 0");
+for (let i = prenom.length - 1; i >= 0; i--) {
+  prenom[i];
+  console.log(prenom[i]);
 }
